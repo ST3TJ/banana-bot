af8c40cb20fff719aa9b6933296ec2f92f301e76
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2024-07-25 23:05:51 +0500

    Changed commands text

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ bot = TeleBot('7384564003:AAH2iM8jOuPzyCdJEzXYdB_4b6T8q3Kijak')
 host = 'https://http.cat/'
 
 bot.set_my_commands([
-    types.BotCommand('/start', 'СТАРТУЕМ'),
-    types.BotCommand('/random', 'ПОЛУЧАЕМ СМЕШНУЮ КАРТИНКУ С КАТОМ ( +AURA +ВАЙБ +ДЕНЬГИ +КОТ +ФУМО )'),
+    types.BotCommand('/start', 'Старт'),
+    types.BotCommand('/random', 'Случайная картинка'),
 ])
 
 http_cat_images = [100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431, 444, 450, 451, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 521, 523, 525, 599]
@@ -16,29 +16,32 @@ http_cat_images = [100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 3
 @bot.message_handler(func=lambda message: True)
 def handle_message(msg: types.Message):
     text = msg.text.lower()
+    chat_id = msg.chat.id
 
     def send_photo(image_url):
-        bot.send_photo(msg.from_user.id, image_url)
+        bot.send_photo(chat_id, image_url)
 
     def send_message(text):
-        bot.send_message(msg.from_user.id, text)
+        bot.send_message(chat_id, text)
 
     if text.startswith('/'):
         if text == '/random':
             index = choice(http_cat_images)
             send_photo(f"{host}{index}")
         elif text == '/start':
-            send_message('КУДА СТАРТУЕМ ТО')
+            send_message('Введите ошибку чтобы узнать что она значит')
         else:
-            send_message('НЕТУ ТАКОЙ КОМАНДЫ ДЕГЕНЕРАТ')
+            send_message('Такой команды нет')
     else:
         try:
             response = requests.get(f"{host}{text}")
             if response.status_code == 200:
                 send_photo(response.url)
             else:
-                send_message('НЕТУ ТАКОГО СОСИТЕ')
+                send_message('Такой ошибки не существует')
         except requests.RequestException:
-            send_message('ОШИБКА В СЕТИ')
+            send_message('Ошибка сети')
 
 bot.polling(non_stop=True, interval=0)
+
+print('Bot sucessfully started')
