66b7acf2107920db22794438465a7b4d133d0b07
ST3TJ <89393349+ST3TJ@users.noreply.github.com>
2024-07-25 12:31:34 +0500

    Ponos bomza

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from telebot import TeleBot
-
 bot = TeleBot('7384564003:AAH2iM8jOuPzyCdJEzXYdB_4b6T8q3Kijak')
 
-def msg_handler(*args, **kwargs):
-    print(*args, **kwargs)
+@bot.message_handler()
+def get_text_messages(msg):
+    bot.send_message(msg.from_user.id, 'Соси)')
 
-bot.add_message_handler(msg_handler)
+bot.polling(none_stop=True, interval=0)
